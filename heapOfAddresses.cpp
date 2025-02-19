01df51c79c3318e03006d86db1a4e0c3406063ff
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-02-19 14:42:30 +0000

    commit

---
+++ heapOfAddresses.cpp
@@ -0,0 +1,20 @@
+#ifndef ADDRESS_S_EXITS
+#define ADDRESS_S_
+
+#include <string>
+
+class Address{
+	protected:
+		std::string street;
+		std::string city;
+		std::string state;
+		std::string zip;
+	
+	public:
+		Address();
+		void init (std::string street, std::string city,
+				std::string state, std::string zip;)
+		void printAddress();
+		};
+#endif
+
