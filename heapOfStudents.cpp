51259f03472f23f76c033a355b63fa56346393ce
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-02-21 14:18:00 +0000

    commit

--- heapOfStudents.cpp
+++ heapOfStudents.cpp
@@ -5,3 +5,21 @@
 
 class Student{
 	protected:
+		std::string firstName;
+		std::string lastName;
+		std::string street;
+		std::string city;
+		std::string state;
+		std::string zip;
+		std::string birthDate;
+		std::string expectedGradDate;
+		std::string creditHoursCompleted;
+	public:
+		Student()
+		void init (std::string firstName, std::string lastName,
+					std::string street, std::string city,
+					std::string state, std::string zip,
+					std::string birthDate, std::string expectedGradDate,
+					std::string creditHoursCompleted;)
+		void printStudent();
+		};
