e11084487b18c329ce869a35c3f35e198d6d7483
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-03-20 16:50:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,3 +2,37 @@
 #include "heapOfDates.h"
 #include "heapOfAddresses.h"
 #include "heapOfStudents.h"
+
+void testAddress();
+void testDate();
+void testStudent();
+
+int main(){
+  std::cout << "Hello!" << std::endl;
+  testAddress();
+  testDate();
+  testStudent();
+  return 0;
+} // end main
+
+void testAddress(){
+  heapOfAddresses a;
+  a.init("123 W Main St", "Muncie", "IN", "47303");
+  a.printAddress();
+} // end testAddress
+
+void testDate(){
+ heapOfDates d;
+ d.init("01/27/1997");
+ d.printDate();
+} // end testDate
+
+void testStudent(){
+  std::string studentString = "Danielle,Johnson,32181 Johnson Course Apt. 389,New Jamesside,IN,59379,02/17/2004,05/15/2027,65";
+  Student* student = new Student();
+  student->init(studentString);
+  student->printStudent();
+  std::cout << std::endl;
+  std::cout << student->getLastFirst();
+  delete student;
+} // end testStudent
